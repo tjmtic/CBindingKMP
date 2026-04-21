a48f492062d1d674acc610f6921601c374563aa1
TM <tjmcard@gmail.com>
2026-04-21 05:21:35 -0700

    Article Html

--- website/vite.config.js
+++ website/vite.config.js
@@ -5,5 +5,11 @@ export default defineConfig({
     base: './',
     build: {
         outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                article: 'article.html'
+            }
+        }
     }
 })
